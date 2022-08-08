2285ca0c9bad6e4bd459f9502aa747d87510f43c
Halis Manaz <halismanaz@hotmail.com>
2022-08-08 18:02:36 +0300

    User struct added

--- users.go
+++ users.go
@@ -1,1 +1,9 @@
 package main
+
+type User struct {
+	ID           string  `json:"ID"`
+	UserName     string  `json:"UserName"`
+	Type         string  `json:"Type"`
+	Streak       int     `json:"Streak"`
+	MonthlyTotal float64 `json:"MonthlyTotal"`
+}
